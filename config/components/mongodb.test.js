66b311ea48cbb06207fe8fe0039b25b9531f420a
kwabena boadu <shadrack.boadu@gmail.com>
2017-04-04 14:02:35 +0000

    Wrap mongodb config test in anonymous callback

--- config/components/mongodb.test.js
+++ config/components/mongodb.test.js
@@ -1,35 +1,36 @@
-'use strict'
+(function () {
+  'use strict'
 
-const chai = require('chai')
-const expect = chai.expect
-// const assert = require('assert')
+  const chai = require('chai')
+  const expect = chai.expect
 
-describe('MongoDB Component', () => {
-  it('should throw error if DB_URL env is undefined', (done) => {
-    try {
-      require('./mongodb')
-    } catch (e) {
-      expect(e.message).to.equal('Config validation error: child "DB_URL" fails because ["DB_URL" is required]')
-      done()
-    }
+  describe('MongoDB Component', () => {
+    it('should throw error if DB_URL env is undefined', (done) => {
+      try {
+        require('./mongodb')
+      } catch (e) {
+        expect(e.message).to.equal('Config validation error: child "DB_URL" fails because ["DB_URL" is required]')
+        done()
+      }
+    })
+
+    it('should throw error if DB_URL scheme is not mongodb', (done) => {
+      process.env.DB_URL = 'mysql://localhost:3000/test'
+
+      try {
+        require('./mongodb')
+      } catch (e) {
+        expect(e.message).to.equal('Config validation error: child "DB_URL" fails because ["DB_URL" must be a valid uri with a scheme matching the mongodb pattern]')
+        done()
+      }
+    })
+
+    it('should return mongodb', () => {
+      process.env.DB_URL = 'mongodb://localhost:3000/test'
+
+      const mongodb = require('./mongodb')
+
+      expect(mongodb.DB_URL).to.equal(process.env.DB_URL)
+    })
   })
-
-  it('should throw error if DB_URL scheme is not mongodb', (done) => {
-    process.env.DB_URL = 'mysql://localhost:3000/test'
-
-    try {
-      require('./mongodb')
-    } catch (e) {
-      expect(e.message).to.equal('Config validation error: child "DB_URL" fails because ["DB_URL" must be a valid uri with a scheme matching the mongodb pattern]')
-      done()
-    }
-  })
-
-  it('should return mongodb', () => {
-    process.env.DB_URL = 'mongodb://localhost:3000/test'
-
-    const mongodb = require('./mongodb')
-
-    expect(mongodb.DB_URL).to.equal(process.env.DB_URL)
-  })
-})
+})()
