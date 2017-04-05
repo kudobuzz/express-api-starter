da4781e02507fdd3b7d5edef7a8ab60d7426e043
kwabena boadu <shadrack.boadu@gmail.com>
2017-04-05 13:54:44 +0000

    Fix broken api tests

--- config/api.js
+++ config/api.js
@@ -5,4 +5,4 @@ const components = require('./components')
 /**
  * Module exports
  */
-module.exports = Object.assign({}, components)
+module.exports = Object.assign({}, components.mongodb, components.server, components.config.logger)
--- config/api.test.js
+++ config/api.test.js
@@ -7,13 +7,13 @@ describe('Api Config Component', () => {
   const moduleName = './api'
 
   beforeEach(() => {
-        // when running all tests we don't want env variables from other tests to contaminate our test data
+    // when running all tests we don't want env variables from other tests to contaminate our test data
     delete process.env.DB_URL
     delete process.env.LOG_LEVEL
     delete process.env.LOG_ENABLED
     delete process.env.PORT
 
-        // Remove any cached instance of the module before each test
+    // Remove any cached instance of the module before each test
     decache('./mongodb')
     decache('./server')
     decache('./logger')
@@ -28,18 +28,10 @@ describe('Api Config Component', () => {
     const config = require(moduleName)
 
     expect(config).to.deep.equal({
-      mongodb: {
-        DB_URL: process.env.DB_URL
-      },
-      server: {
-        PORT: port
-      },
-      config: {
-        logger: {
-          level: 'info',
-          enabled: true
-        }
-      }
+      DB_URL: process.env.DB_URL,
+      PORT: port,
+      level: 'info',
+      enabled: true
     })
   })
 })
