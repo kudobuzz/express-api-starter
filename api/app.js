1136010ad149f461c8377081ca5294f777285d86
Zyed <zyedmohammedanees@gmail.com>
2017-04-14 19:38:31 +0530

    move app.js under api/

--- api/app.js
+++ api/app.js
@@ -1,14 +1,42 @@
 'use strict'
 
 const express = require('express')
-const cors = require('cors')
 const bodyParser = require('body-parser')
 
 const app = express()
 
-module.exports = _ => {
-  app.use(bodyParser.json())
-  app.use(cors())
+app.use(bodyParser.json())
 
-  return app
-}
+app.get('/api/todos', function (req, res) {
+  res.status(200).json([{
+    title: 'remove task on github',
+    completed: false,
+    id: 'dfggdfgkdg'
+  }])
+})
+
+app.post('/api/todo/create', function (req, res) {
+  res.status(200).json({
+    title: req.body.title,
+    completed: false,
+    id: 'rr8r88r8'
+  })
+})
+
+app.put('/api/todo/:todo_id', function (req, res) {
+  res.status(200).json({
+    title: 'some title',
+    completed: req.body.completed,
+    id: req.params.todo_id
+  })
+})
+
+app.get('/api/todo/:todo_id', function (req, res) {
+  res.status(200).json({
+    title: 'some title',
+    completed: false,
+    id: req.params.todo_id
+  })
+})
+
+module.exports = app
--- api/todos/routes.test.js
+++ api/todos/routes.test.js
@@ -1,4 +1,4 @@
-  const app = require('../../lib/app.js')
+  const app = require('../app.js')
   const request = require('supertest')
 
   describe('Todo tests', function () {
