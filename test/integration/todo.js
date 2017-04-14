14f1e43fcbf23cba37989a6966ec4cd8a9777e09
Zyed <zyedmohammedanees@gmail.com>
2017-04-14 06:54:47 +0530

    fix prepush errors

--- test/integration/todo.js
+++ test/integration/todo.js
@@ -8,40 +8,40 @@ describe('Todo tests', function () {
     request(app)
             .get('/api/todos')
             .end(function (err, res) {
-                expect(res.body).to.be.json;
-                expect(Array.isArray(res.body)).to.be.true;
-                done(err);
-            });
-  });
+              expect(res.body).to.be.json;
+              expect(Array.isArray(res.body)).to.be.true;
+              done(err)
+            })
+  })
 
-  it('/api/todo/create to accept a title and return a todo object',function(done){
-     request(app)
+  it('/api/todo/create to accept a title and return a todo object', function (done) {
+    request(app)
            .post('/api/todo/create')
-           .send({title : "some title"})
-           .end(function(err,res){
-                expect(res.body).to.be.json;
-                expect(res.body.title).to.equal('some title');
-                done(err);	
-           });
-  });
+           .send({title: 'some title'})
+           .end(function (err, res) {
+             expect(res.body).to.be.json;
+             expect(res.body.title).to.equal('some title')
+             done(err)
+           })
+  })
 
-  it('/api/todo/todo_id updates the existing todo object',function(done){
-     request(app)
+  it('/api/todo/todo_id updates the existing todo object', function (done) {
+    request(app)
           .put('/api/todo/1234')
-          .send({completed : true})
-          .end(function(err,res){
-          	expect(res.body.completed).to.equal(true);
-          	expect(res.body.id).to.equal('1234');
-          	done(err);
+          .send({completed: true})
+          .end(function (err, res) {
+            expect(res.body.completed).to.equal(true)
+            expect(res.body.id).to.equal('1234')
+            done(err)
           })
   })
 
-  it('/api/todo/todo_id returns the existing todo object',function(done){
-     request(app)
+  it('/api/todo/todo_id returns the existing todo object', function (done) {
+    request(app)
          .get('/api/todo/1234')
-         .end(function(err,res){
-             expect(res.body.id).to.equal('1234');
-             done(err);
-         });
-  });
-});
+         .end(function (err, res) {
+           expect(res.body.id).to.equal('1234')
+           done(err)
+         })
+  })
+})
