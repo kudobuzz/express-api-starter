2359829b7ffc07264c5abf34957355bfd6cb6d06
Zyed <zyedmohammedanees@gmail.com>
2017-04-17 13:59:48 +0530

    remove assert and use chai.expect for assertions;

--- models/todos/schema.test.js
+++ models/todos/schema.test.js
@@ -1,17 +1,27 @@
 const mongoose = require('mongoose')
-const assert = require('assert')
+const expect = require('chai').expect
 const todoSchema = require('./schema.js')
 
 describe('Todo Schema', function () {
   it('Title is required string field', function () {
-    const stringField = todoSchema.path('title')
-    assert.ok(stringField instanceof mongoose.Schema.Types.String)
-    assert.ok(stringField.isRequired)
+    const titleField = todoSchema.path('title')
+    expect(titleField).to.be.instanceof(mongoose.Schema.Types.String)
+    expect(titleField.isRequired).to.equal(true)
   })
 
   it('Completed is a boolean field default to false', function () {
-    const stringField = todoSchema.path('completed')
-    assert.ok(stringField instanceof mongoose.Schema.Types.Boolean)
-    assert.equal(stringField.defaultValue, false, 'completed should default to false')
+    const completedField = todoSchema.path('completed')
+    expect(completedField).to.be.instanceof(mongoose.Schema.Types.Boolean)
+    expect(completedField.default()).to.equal(false)
+  })
+
+  it('model is created with correct defaults', function () {
+    let TestModel = mongoose.model('Test', todoSchema)
+    let options = {
+      title: 'testing'
+    }
+    let todo = new TestModel(options)
+    expect(todo.title).to.equal(options.title)
+    expect(todo.completed).to.equal(false)
   })
 })
