166041c6385887ed966ba80e254f6b8469cfe8f5
Samuel Amoah <snamoah@gmail.com>
2018-03-12 13:30:13 +0000

    test(todos): write tests for todo schema

---
+++ models/todos/schema/index.test.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const mongoose = require('@okudobuzz/mongodb-utils').getMongoose()
+const { expect } = require('chai')
+const { schema } = require('./index')
+
+describe('Todo Schema', () => {
+  it('Title is required string field', () => {
+    const titleField = schema.path('title')
+    expect(titleField).to.be.instanceof(mongoose.Schema.Types.String)
+    expect(titleField.isRequired).to.equal(true)
+  })
+
+  it('Completed is a boolean field default to false', () => {
+    const completedField = schema.path('completed')
+    expect(completedField).to.be.instanceof(mongoose.Schema.Types.Boolean)
+    expect(completedField.default()).to.equal(false)
+  })
+
+  it('model is created with correct defaults', async () => {
+    let TestModel = mongoose.model('Test', schema)
+    let options = {
+      title: 'testing'
+    }
+    let todo = new TestModel(options)
+    expect(todo.title).to.equal(options.title)
+    expect(todo.completed).to.equal(false)
+  })
+})
