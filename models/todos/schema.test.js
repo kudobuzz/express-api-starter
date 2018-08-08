a1932757bd2c54b698917a3961b1f39b552d098f
Samuel Amoah <snamoah@gmail.com>
2018-08-08 11:45:22 +0000

    refactor(db): use mongodb-utils method for initializing db

--- models/todos/schema.test.js
+++ models/todos/schema.test.js
@@ -4,20 +4,20 @@ const { expect } = require('chai')
 const mongoose = require('mongoose')
 const { schema: todoSchema } = require('./schema')
 
-describe('Todo Schema', ()  => {
-  it('Title is required string field', ()  => {
+describe('Todo Schema', () => {
+  it('Title is required string field', () => {
     const titleField = todoSchema.path('title')
     expect(titleField.isRequired).to.equal(true)
     expect(titleField).to.be.instanceof(mongoose.Schema.Types.String)
   })
 
-  it('Completed is a boolean field default to false', () =>  {
+  it('Completed is a boolean field default to false', () => {
     const completedField = todoSchema.path('completed')
     expect(completedField).to.be.instanceof(mongoose.Schema.Types.Boolean)
     expect(completedField.default()).to.equal(false)
   })
 
-  it('model is created with correct defaults', () =>  {
+  it('model is created with correct defaults', () => {
     let TestModel = mongoose.model('Test', todoSchema)
     let options = {
       title: 'testing'
