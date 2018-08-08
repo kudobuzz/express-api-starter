61adca731f6921c6b4727ac64b129337fcc66749
Samuel Amoah <snamoah@gmail.com>
2018-08-08 11:29:20 +0000

    refactor(todos): cleanup code in schema and test

--- models/todos/schema.test.js
+++ models/todos/schema.test.js
@@ -1,21 +1,23 @@
-const mongoose = require('mongoose')
-const expect = require('chai').expect
-const todoSchema = require('./schema.js')
+'use strict'
 
-describe('Todo Schema', function () {
-  it('Title is required string field', function () {
+const { expect } = require('chai')
+const mongoose = require('mongoose')
+const { schema: todoSchema } = require('./schema')
+
+describe('Todo Schema', ()  => {
+  it('Title is required string field', ()  => {
     const titleField = todoSchema.path('title')
-    expect(titleField).to.be.instanceof(mongoose.Schema.Types.String)
     expect(titleField.isRequired).to.equal(true)
+    expect(titleField).to.be.instanceof(mongoose.Schema.Types.String)
   })
 
-  it('Completed is a boolean field default to false', function () {
+  it('Completed is a boolean field default to false', () =>  {
     const completedField = todoSchema.path('completed')
     expect(completedField).to.be.instanceof(mongoose.Schema.Types.Boolean)
     expect(completedField.default()).to.equal(false)
   })
 
-  it('model is created with correct defaults', function () {
+  it('model is created with correct defaults', () =>  {
     let TestModel = mongoose.model('Test', todoSchema)
     let options = {
       title: 'testing'
