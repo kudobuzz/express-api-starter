7f9dd395e0a359533a42a758bfc12c102e9152d6
owitia <owitigracie@gmail.com>
2019-04-02 11:36:50 +0300

    added api implementation

--- api/server.js
+++ api/server.js
@@ -1,13 +1,28 @@
 'use strict'
-const app = require('./app')()
-const config = require('../config')
 
-const port = config('PORT')
+const { start } = require('@kudobuzz/resource-manager')
+const app = require('./app')
+const config = require('../config')
+const log = require('../lib/log')().child({ type: 'api/server' })
 
 let server
-const serverReady = _ => {
+const PORT = config('PORT')
+
+const onReady = _ => {
     if (server) return server
   
-    server = app.listen(port, _ => log.info(`server running on port ${PORT}`))
+    server = app.listen(PORT, _ => log.info(`server running on port ${PORT}`))
   }
-module.exports = serverReady
+
+const onShutDown = _ => {
+    if (server) {
+      log.info('shutting down server on port 3000')
+      return server.close(err => {
+        if (err) {
+          log.err(err)
+        }
+      })
+    }
+}
+
+start({ resources: onReady, onShutDown })
