85fd6aee8e8a107b2ba56e254dd7be70719bdd54
owitia <owitigracie@gmail.com>
2019-04-09 14:01:07 +0300

    added config files

---
+++ tests/utils/index.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const mongoose = require('mongoose')
+const request = require('supertest')
+
+const config = require('../../config')
+const app = require('../../api/app')()
+const db = require('../../lib/resources/mongodb')()
+const isConnected = require('../../lib/resources/mongodb').isConnected
+
+module.exports = {
+  dbSetup: async () => {
+    if (!isConnected()) {
+      await db.connect(config('MONGODB_URL'))
+    }
+  },
+  api () {
+    return request(app)
+  },
+  tearDown: async () => {
+    await mongoose.connections.dropDatabase()
+  },
+  async closeMongoDB () {
+    await mongoose.connection.close()
+  }
+}
